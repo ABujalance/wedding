7981e68e1beee233048cc47abcc4617020e5293b
abujalance <amuoz@uw.co.uk>
2025-08-06 17:31:30 +0200

    fix: page build

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,13 +3,11 @@ import { Stack, Typography } from '@mui/material';
 import { redirect } from 'next/navigation';
 import { PageLayout } from '../PageLayout';
 import { GuestList } from './components/GuestList';
+import { PageProps } from '../../../.next/types/app/layout';
 
-interface AdminPageProps {
-  searchParams?: { [key: string]: string | string[] | undefined };
-}
-
-export default async function Admin({ searchParams }: AdminPageProps) {
-  const adminTokenId = await searchParams?.adminTokenId;
+export default async function Admin({ searchParams }: PageProps) {
+  const adminTokenId = ((await searchParams) as { adminTokenId: string })
+    .adminTokenId;
 
   if (process.env.ADMIN_TOKEN_ID !== adminTokenId) {
     redirect('/');
